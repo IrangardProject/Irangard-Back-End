70f649285854d7688311b568e9e672fee07f070a
Ghazal <ghazal.best76@gmail.com>
2022-03-09 07:17:57 +0330

    Auth User Model

--- Irangard/accounts/models.py
+++ Irangard/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(
+        upload_to='images/users/', blank=True, null=True)
+    phone_no = models.CharField(max_length=11, null=True, blank=True)
+    is_special = models.BooleanField(default=False, blank=True)
+
+# class Verification(models.Model):
+#     email = models.EmailField(primary_key=True)
+#     token = models.CharField(max_length=6)
+#     create_time = models.DateTimeField(auto_now_add=True)
