739f8288198b14d96f78ce7359f8d0e347a092f8
Ghazal <ghazal.best76@gmail.com>
2022-03-25 19:03:59 +0430

    small model test

--- Irangard/accounts/tests/test_models.py
+++ Irangard/accounts/tests/test_models.py
@@ -1,2 +1,27 @@
 from django.test import TestCase
 from ..models import *
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        
+        User.objects.create(
+            email = "ghazal@gmail.com",
+            username = "Ghazal",
+            first_name = "غزل",
+            last_name = "بخشنده",
+            phone_no = "09225678765",
+            password = '123456',
+        )
+
+    def test_user_email(self):
+        user = User.objects.get(email='Ghazal')
+        self.assertEqual(user.email, 'ghazal@gmail.com')
+
+    def test_user_phone_no(self):
+        user = User.objects.get(email='ghazal@gmail.com')
+        self.assertEqual(user.phone_no, '09225678765')
+    
+    def test_user_password(self):
+        user = User.objects.get(email='Ghazal')
+        self.assertEqual(user.password, '123456')
--- Irangard/accounts/tests/test_views.py
+++ Irangard/accounts/tests/test_views.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from rest_framework import status
+from django.urls import reverse
 from ..models import *
+
+client = Client()
+
+# class ActivateTestCase(TestCase):
+
+
+# class SignUpTestCase(TestCase):
+
+
+# class LoginTestCase(TestCase):
+
+
