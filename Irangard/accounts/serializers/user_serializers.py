6f113f2622294ec312e07b107e11de91dc35c98c
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2022-04-20 22:29:11 +0430

    [feat] add image field to user profile api and fix some bugs for fullname

--- Irangard/accounts/serializers/user_serializers.py
+++ Irangard/accounts/serializers/user_serializers.py
@@ -15,12 +15,18 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'image', 'about_me', 'is_special', 'full_name']
+        read_only_fields = ('email',)
         
     def get_fullname(self, user):
         first_name = str(user.first_name)
         last_name = str(user.last_name)
         full_name = first_name + ' ' + last_name
         return full_name
+    
+   
+        
+    
+    
     
         
 
