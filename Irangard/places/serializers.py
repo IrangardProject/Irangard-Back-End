ab9344a8d3e1c07b15119a226a6abfb2396c1bb3
Ghazal <ghazal.best76@gmail.com>
2022-04-22 18:19:43 +0430

    place models

---
+++ Irangard/places/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from accounts.models import User
+from datetime import date, datetime, time, timedelta
+
+
+
