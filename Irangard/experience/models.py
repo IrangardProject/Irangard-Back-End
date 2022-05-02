192f4f21336762180dc40048108871e3f638656f
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2022-05-03 00:21:37 +0430

    [feat] add experience app and model

--- Irangard/experience/models.py
+++ Irangard/experience/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Irangard.accounts.models import User
+from Irangard.places.models import Place
 
-# Create your models here.
+class Experience(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True, null=True)
+    likeNumber = models.IntegerField(default=0)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='experiences')
+    
+# class Like(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+#     experience = models.ForeignKey(Experience, on_delete=models.CASCADE, related_name='likes')
+    
+    
