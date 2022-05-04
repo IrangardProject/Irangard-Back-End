2afaf679b0732b3f79854ca02a39b0fcd0dc0f1d
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2022-05-04 11:43:23 +0430

    [fix] change experience model

--- Irangard/experience/models.py
+++ Irangard/experience/models.py
@@ -4,12 +4,17 @@ from places.models import Place
 
 class Experience(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField(blank=True, null=True)
-    likeNumber = models.IntegerField(default=0)
+    image = models.CharField()
+    like_number = models.IntegerField(default=0)
+    comment_number = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    summary = models.TextField(blank=True, null=True, max_length=500)
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='experiences')
+    date_created = models.CharField(max_length=255)
     
-# class Like(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
-#     experience = models.ForeignKey(Experience, on_delete=models.CASCADE, related_name='likes')
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE, related_name='likes')
     
     
