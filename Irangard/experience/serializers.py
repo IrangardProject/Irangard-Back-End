7fed20678e168418ab24b71dff1567a3ccc59cc9
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2022-05-04 22:27:30 +0430

    [feat] add some fields to experience serializer

--- Irangard/experience/serializers.py
+++ Irangard/experience/serializers.py
@@ -2,8 +2,27 @@ from rest_framework import serializers
 from .models import Experience
 
 class ExperienceSerializer(serializers.ModelSerializer):
+    
+    place_title = serializers.SerializerMethodField('get_place_title')
+    user_username = serializers.SerializerMethodField('get_user_username')
+    user_image = serializers.SerializerMethodField('get_user_image')
+    
     class Meta:
         model = Experience
-        fields = "__all__"
-        read_only_fields = ['like_number', 'comment_number', 'views', 'rate', 'rate_no', ]
+        fields = "__all__"  
+        read_only_fields = ['like_number', 'comment_number', 'views', 'rate', 'rate_no', 'get_place_title', 'get_user_username', 'get_user_image']
         
+        
+    def get_place_title(self, experience):
+        place = experience.place
+        return place.title
+    
+    def get_user_username(self, experience):
+        user = experience.user
+        return user.username
+    
+    def get_user_image(self, experience):
+        user = experience.user
+        return user.image.url
+    
+    
--- Irangard/experience/views.py
+++ Irangard/experience/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ExperienceSerializer
 from .models import Experience
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 
 
 class ExperienceViewSet(ModelViewSet):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get(self, request, username, *args, **kwargs):
+    
+        parser_classes = [MultiPartParser, FormParser]
+        serializer = ExperienceSerializer(context = {'user': request.user})
+        return Response(serializer.data)
+    
     
