df361d9c433137988d9401be494787505fb99544
Ghazal <ghazal.best76@gmail.com>
2022-05-08 03:37:08 +0430

    change serializer

--- Irangard/experience/serializers.py
+++ Irangard/experience/serializers.py
@@ -60,20 +60,13 @@ class ReplySerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    reply = ReplySerializer(read_only=True, many=True)
-    user = UserCommentSerializer(read_only=True)
-    class Meta:
-        model = Comment
-        fields = ['id', 'created_date', 'text', 'user', 'reply']
-        read_only_fields = ['id', 'created_date']
+class CommentSerializer(ReplySerializer):
+    replies = ReplySerializer(read_only=True, many=True)
+    class Meta(ReplySerializer.Meta):
+        fields = super().fields + ['replies']
 
     def create(self, validated_data):
         request = self.context.get("request")
         validated_data['experience_id'] = self.context.get("experience")
         validated_data['user'] = request.user
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        validated_data['created_date'] = datetime.now()
-        return super().update(instance, validated_data)
+        return serializers.ModelSerializer.create(validated_data)
