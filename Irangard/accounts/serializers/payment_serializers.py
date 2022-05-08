c3a8874264f2d4d2e224933bbccb78d7e1ba4ec3
Emadmousavi <emadmoosavi79@gmail.com>
2022-05-09 02:33:27 +0430

    [chore] minor changes

--- Irangard/accounts/serializers/payment_serializers.py
+++ Irangard/accounts/serializers/payment_serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 
 
 class VerifiedPaymentSerializer(serializers.Serializer):
-    status = serializers.IntegerField(ReadOnly = True)
-    track_id = serializers.IntegerField(ReadOnly = True)
-    order_id = serializers.CharField(max_length = 50,ReadOnly = True)
-    amount = serializers.IntegerField(ReadOnly = True)
-    date = serializers.DateField(ReadOnly = True)
+    status = serializers.IntegerField(blank=True)
+    track_id = serializers.IntegerField(blank = True)
+    order_id = serializers.CharField(max_length = 50, blank = True)
+    amount = serializers.IntegerField(blank = True)
+    date = serializers.DateField(blank = True)
