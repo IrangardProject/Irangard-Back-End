97cd244dc78d41c730e1cd10d14f774f9d3b311f
Emadmousavi <emadmoosavi79@gmail.com>
2022-05-08 17:16:50 +0430

    [fix] fix payment api

--- Irangard/accounts/views.py
+++ Irangard/accounts/views.py
@@ -1,13 +1,82 @@
+import requests
+import json
+import uuid
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.decorators import action, api_view, permission_classes
+from .models import StagedPayments
+from django.contrib.auth import authenticate, login
 
-class Pay(APIView):
-    
+
+class PayViewSet(GenericViewSet):
+
     permission_classes = [permissions.AllowAny]
 
-    def post(self, request):
-        
-        return Response(f"sent webhook info is :{request.data} ",status=status.HTTP_200_OK)
+    @action(detail=False, url_path='pay', methods=['POST'], permission_classes=[permissions.AllowAny])
+    def pay(self, request):
+
+        username = 'emad12'
+        password = 'emad1234'
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+        order_id = str(uuid.uuid4())
+        my_data = {
+            "order_id": order_id,
+            "amount": 50000,
+            "name": f"{request.user.username}",
+            "mail": f"{request.user.email}",
+            "callback": "https://api.irangard.ml/accounts/pay/verify/"
+        }
+
+        my_headers = {"Content-Type": "application/json",
+                      'X-API-KEY': '3394842f-7407-4598-8c48-499a15c8d0b7',
+                      'X-SANDBOX': '0'}
+
+        response = requests.post(url="https://api.idpay.ir/v1.1/payment", data=json.dumps(my_data),
+                                 headers=my_headers)
+        response.raise_for_status()
+        print(response.status_code)
+        try:
+            obj = StagedPayments.objects.get(user=request.user)
+            obj.transaction_id = json.loads(response.content)['id']
+            obj.order_id = order_id
+            obj.save()
+
+        except StagedPayments.DoesNotExist:
+            obj = StagedPayments.objects.create(transaction_id=json.loads(response.content)[
+                'id'], order_id=order_id, user=request.user)
+            obj.save()
+
+        return Response(f"{json.loads(response.content)}", status=status.HTTP_200_OK)
+
+    @action(detail=False, url_path='verify', methods=['POST'], permission_classes=[permissions.AllowAny])
+    def verify(self, request):
+
+        username = 'emad12'
+        password = 'emad1234'
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+        my_data = {
+            "order_id": f'{request.user.staged_payments_info.order_id}',
+            "id": f'{request.user.staged_payments_info.transaction_id}',
+
+        }
+
+        my_headers = {"Content-Type": "application/json",
+                      'X-API-KEY': '3394842f-7407-4598-8c48-499a15c8d0b7',
+                      'X-SANDBOX': '0'
+                      }
+
+        response = requests.post(url="https://api.idpay.ir/v1.1/payment/verify", data=json.dumps(my_data),
+                                 headers=my_headers)
+        response.raise_for_status()
+        print(response.content, ' ', response.status_code)
+        return Response(f"{json.loads(response.content)}", status=status.HTTP_200_OK)
