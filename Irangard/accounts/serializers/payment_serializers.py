8da130eebbcf14e6b2fae67e0a30f098a34c2600
Emadmousavi <emadmoosavi79@gmail.com>
2022-05-10 02:50:49 +0430

    [feat,fix] add admin view to add admin user nad remove specail user & fix payment api

--- Irangard/accounts/serializers/payment_serializers.py
+++ Irangard/accounts/serializers/payment_serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 
 
 class VerifiedPaymentSerializer(serializers.Serializer):
-    status = serializers.IntegerField()
-    track_id = serializers.IntegerField()
-    order_id = serializers.CharField(max_length = 50)
-    amount = serializers.IntegerField()
-    date = serializers.DateField()
+    status = serializers.IntegerField(read_only=True)
+    track_id = serializers.IntegerField(read_only=True)
+    order_id = serializers.CharField(read_only=True)
+    amount = serializers.IntegerField(read_only=True)
+    #date = serializers.DateField(input_formats=)
