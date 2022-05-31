8e8e735e4c4a40b2097247af54374238f2017cbe
Ghazal <ghazal.best76@gmail.com>
2022-05-31 13:39:17 +0430

    fix bug

--- Irangard/experience/views.py
+++ Irangard/experience/views.py
@@ -79,7 +79,7 @@ class ExperienceViewSet(ModelViewSet):
 	def feed(self, request, *args, **kwargs):
 		user = request.user
 		serializer = ExperienceSerializer(
-			[follower.experiences for follower in user.followers], many=True)
+			[follower.experiences for follower in user.followers.all()], many=True)
 		return Response(status=status.HTTP_200_OK, data=serializer.data)
 
  
