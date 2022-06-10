188077ab5772f59d00d1acd37c6bf76436acf6bd
Emadmousavi <emadmoosavi79@gmail.com>
2022-06-11 01:04:18 +0430

    [fix] fix redist host in settings.py

--- Irangard/Irangard/settings.py
+++ Irangard/Irangard/settings.py
@@ -219,7 +219,7 @@ REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": f"redis://{REDIS_HOST}:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
