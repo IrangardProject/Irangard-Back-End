c6e9b167f98e5ee6c8033a81f5dda02628bfd3ed
Seyyed-Amirhossein-Mirmohammadi <amir.mirmohammadi.2002@gmail.com>
2023-04-17 00:12:54 +0330

    [feat] filter events

--- Irangard/events/filters.py
+++ Irangard/events/filters.py
@@ -1,17 +1,24 @@
 from django_filters.rest_framework import FilterSet
 from .models import Event
-
+from django_filters import rest_framework as filters
 
 
 class EventFilter(FilterSet):
+    tag = filters.CharFilter(method='filter_by_tag')
+    
     class Meta:
         model = Event
         fields = ['province', 'city', 'event_type', 'event_category', 
-                'tags__name', 'title', 'start_date', 'end_date']
+                'title', 'start_date', 'end_date', 'is_free']
         
         fields = {
-            'title': ['contains'], 'event_type': ['exact'], 
-            'event_category': ['exact'], 'start_date': ['lte', 'gte'],
+            'title': ['contains'], 
+            'event_type': ['exact'],
+            'event_category': ['exact'], 
+            'start_date': ['lte', 'gte'],
             'end_date': ['lte', 'gte'],
+            'is_free': ['exact'],
         }
     
+    def filter_by_tag(self, queryset, name, value):
+        return queryset.filter(tags__name=value)
