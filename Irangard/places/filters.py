31173d558c2386e88aa80619ab06638dd182f74e
Seyyed-Amirhossein-Mirmohammadi <amir.mirmohammadi.2002@gmail.com>
2023-04-17 00:45:04 +0330

    [fix] place filter

--- Irangard/places/filters.py
+++ Irangard/places/filters.py
@@ -1,22 +1,27 @@
-
 from django_filters.rest_framework import FilterSet
-from .models import Place, Contact
+from .models import Place
 import django_filters
-
-
+from django_filters import rest_framework as filters
 
 
 class PlaceFilter(FilterSet):
     province = django_filters.CharFilter(field_name="contact__province", lookup_expr='exact')
     city = django_filters.CharFilter(field_name="contact__city", lookup_expr='exact')
-
+    tag = filters.CharFilter(method='filter_by_tag')
+    
     class Meta:
         model = Place
         fields = ['province', 'city', 'place_type', 'is_free', 
-                'tags__name', 'features__title', 'rooms__capacity']
+                'features__title', 'rooms__capacity']
         
         fields = {
             'rooms__price': ['lte', 'gte'],
             'optional_costs__price': ['lte', 'gte'],
-            'rate': ['gte'], 'title': ['contains'], 'place_type': ['exact']
+            'rate': ['gte'], 
+            'title': ['contains'],
+            'place_type': ['exact'],
+            'is_free': ['exact'],
         }
+    
+    def filter_by_tag(self, queryset, name, value):
+        return queryset.filter(tags__name=value)
