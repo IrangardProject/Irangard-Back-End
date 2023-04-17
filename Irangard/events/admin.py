f3f169c8b65cebd8fb6827847767767c4a311295
Seyyed-Amirhossein-Mirmohammadi <amir.mirmohammadi.2002@gmail.com>
2023-04-17 22:48:53 +0330

    [fix] serializers

--- Irangard/events/admin.py
+++ Irangard/events/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Tag, Image
 
 
 admin.site.register(Event)
+admin.site.register(Tag)
+admin.site.register(Image)
