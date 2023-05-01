59426698cfde2b680f1dd9361000838ee856280c
ahmadreza <ahmadreza.tmb@gmail.com>
2023-05-02 00:07:04 +0330

    [feat] delete extra lines

--- Irangard/emails/models.py
+++ Irangard/emails/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 from utils.constants import EMAIL_STATES
 
-"""
-email = EmailMessage('تور جدید در ایرانگرد منتظر شماست !',
-                                     self.get_tour_notification_email_template(user),
-                                     settings.EMAIL_HOST_USER,
-                                     [user.email]
-                                     )
-"""
 
 class EmailQueue(models.Model):
     email_title = models.CharField(max_length=256)
