95383f195720176737dc540090b8545a7594680f
Seyyed-Amirhossein-Mirmohammadi <amir.mirmohammadi.2002@gmail.com>
2023-05-02 17:43:01 +0330

    [fix] improve codes by inheritence

--- Irangard/suggestion/views.py
+++ Irangard/suggestion/views.py
@@ -8,76 +8,48 @@ from rest_framework import status
 from .permissions import SuggestionPermission
 
 
-class TourSuggstionViewSet(ModelViewSet):
+class SuggestionViewSet(ModelViewSet):
+    permission_classes = [SuggestionPermission]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['sender'] = self.request.user
+        data = serializer.validated_data
+        if data['sender'] == data['receiver']:
+            return Response({'error': 'Sender and receiver cannot be the same.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if self.SuggestionModel.objects.filter(receiver=data['receiver'], sender=data['sender'], **self.suggestion_fields(data)):
+            return Response(
+                {'error': f'You have been suggested this {self.suggestion_name} to this user before'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class TourSuggestionViewSet(SuggestionViewSet):
     queryset = TourSuggestion.objects.all()
     serializer_class = TourSuggestionSerializer
-    permission_classes = [SuggestionPermission]
-    
-    
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.validated_data['sender'] = self.request.user
-        data = serializer.validated_data
-        if data['sender'] == data['receiver']:
-            return Response({'error': 'Sender and receiver cannot be the same.'}, 
-                        status=status.HTTP_400_BAD_REQUEST)
-        
-        if TourSuggestion.objects.filter(receiver=data['receiver'], sender=data['sender'], tour=data['tour']):
-            return Response(
-                {'error': 'You have been suggested this tour to this user before'}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
-    
-class EventSuggstionViewSet(ModelViewSet):
+    SuggestionModel = TourSuggestion
+    suggestion_name = 'tour'
+    suggestion_fields = lambda self, data: {'tour': data['tour']}
+
+
+class EventSuggestionViewSet(SuggestionViewSet):
     queryset = EventSuggestion.objects.all()
     serializer_class = EventSuggestionSerializer
-    permission_classes = [SuggestionPermission]
-    
-    
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.validated_data['sender'] = self.request.user
-        data = serializer.validated_data
-        if data['sender'] == data['receiver']:
-            return Response({'error': 'Sender and receiver cannot be the same.'}, 
-                        status=status.HTTP_400_BAD_REQUEST)
-        
-        if EventSuggestion.objects.filter(receiver=data['receiver'], sender=data['sender'], event=data['event']):
-            return Response(
-                {'error': 'You have been suggested this event to this user before'}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    SuggestionModel = EventSuggestion
+    suggestion_name = 'event'
+    suggestion_fields = lambda self, data: {'event': data['event']}
 
 
-class PlaceSuggstionViewSet(ModelViewSet):
+class PlaceSuggestionViewSet(SuggestionViewSet):
     queryset = PlaceSuggestion.objects.all()
     serializer_class = PlaceSuggestionSerializer
-    permission_classes = [SuggestionPermission]
-    
-    
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.validated_data['sender'] = self.request.user
-        data = serializer.validated_data
-        if data['sender'] == data['receiver']:
-            return Response({'error': 'Sender and receiver cannot be the same.'}, 
-                        status=status.HTTP_400_BAD_REQUEST)
-        
-        if PlaceSuggestion.objects.filter(receiver=data['receiver'], sender=data['sender'], place=data['place']):
-            return Response(
-                {'error': 'You have been suggested this place to this user before'}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    SuggestionModel = PlaceSuggestion
+    suggestion_name = 'place'
+    suggestion_fields = lambda self, data: {'place': data['place']}
