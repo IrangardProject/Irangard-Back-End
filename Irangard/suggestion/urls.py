e325ed3615e0369df1783511a197957da268bf84
Seyyed-Amirhossein-Mirmohammadi <amir.mirmohammadi.2002@gmail.com>
2023-05-02 17:42:01 +0330

    [fix] import

--- Irangard/suggestion/urls.py
+++ Irangard/suggestion/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.urls.conf import include
 from rest_framework_nested import routers
-from .views import *
+from .views import TourSuggestionViewSet, EventSuggestionViewSet, PlaceSuggestionViewSet
 
 app_name = 'suggestion'
 
 router = routers.DefaultRouter()
-router.register('tour', TourSuggstionViewSet)
-router.register('event', EventSuggstionViewSet)
-router.register('place', PlaceSuggstionViewSet)
+router.register('tour', TourSuggestionViewSet)
+router.register('event', EventSuggestionViewSet)
+router.register('place', PlaceSuggestionViewSet)
 
 
 urlpatterns = [
