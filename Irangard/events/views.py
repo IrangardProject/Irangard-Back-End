0f560e5cdff627fb597dbb6245b144bdb79aa791
Seyyed-Amirhossein-Mirmohammadi <amir.mirmohammadi.2002@gmail.com>
2023-05-05 01:10:18 +0330

    [feat] recommended events view

--- Irangard/events/views.py
+++ Irangard/events/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpRequest, HttpResponse
 from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .pagination import DefaultPagination
 from .models import Event, Tag, Image
-from .serializers import *
-from .permissions import *
+from .serializers import EventSerializer
+from .permissions import EventPermission
 from .filters import EventFilter
 
 
@@ -61,3 +59,22 @@ class EventViewSet(ModelViewSet):
                 Tag.objects.create(event=event, **tag)
 
         return super().update(request, *args, **kwargs)
+
+        
+    def retrieve(self, request, *args, **kwargs):
+        obj = get_object_or_404(Event, pk=kwargs['pk'])
+        session_key = 'viewed_object_{}'.format(kwargs['pk'])
+        if not request.session.get(session_key, False): 
+            # to prevent a user's multiply view, count multiply times
+            obj.views += 1
+            obj.save()
+            request.session[session_key] = True
+        return super().retrieve(request, *args, **kwargs)
+    
+    
+    @action(methods=['GET'], detail=False)
+    def recommended_events(self, request):
+        not_expired_events = [event for event in Event.objects.all() if not event.is_expired]
+        sorted_events = sorted(not_expired_events, key=lambda t: t.recommendation_rate, reverse=True)
+        serializer = EventSerializer(sorted_events, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
