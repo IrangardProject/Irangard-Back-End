2951c040a0caddf2fd3454a547d6ed6997753c65
Seyyed-Amirhossein-Mirmohammadi <amir.mirmohammadi.2002@gmail.com>
2023-05-16 01:53:50 +0330

    [feat] admin activation for event objects views

--- Irangard/events/views.py
+++ Irangard/events/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .pagination import DefaultPagination
@@ -12,10 +13,11 @@ from .serializers import EventSerializer
 from .permissions import EventPermission
 from .filters import EventFilter
 from django.utils import timezone
+from utils.constants import StatusMode
 
 
 class EventViewSet(ModelViewSet):
-    queryset = Event.objects.filter(end_date__gte=timezone.now())
+    queryset = Event.objects.filter(end_date__gte=timezone.now(), status=StatusMode.ACCEPTED)
     serializer_class = EventSerializer
     filterset_class = EventFilter
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
@@ -79,3 +81,29 @@ class EventViewSet(ModelViewSet):
         sorted_events = sorted(not_expired_events, key=lambda t: t.recommendation_rate, reverse=True)
         serializer = self.get_serializer(sorted_events, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    @action(detail=True, methods=['put'], permission_classes=[IsAdminUser])
+    def admin_acceptance(self, request, pk=None):
+        event = get_object_or_404(Event, pk=pk)
+        if event.status == StatusMode.ACCEPTED:
+            message = f"The event with ID {pk}, was already in accepted status."
+            return Response(status=status.HTTP_200_OK, data={"message": message})
+        
+        event.status = StatusMode.ACCEPTED
+        event.save()
+        message = f"The event with ID {pk}, is now in accepted status."
+        return Response(data={"message": message}, status=status.HTTP_200_OK)
+    
+    
+    @action(detail=True, methods=['put'], permission_classes=[IsAdminUser])
+    def admin_denial(self, request, pk=None):
+        event = get_object_or_404(Event, pk=pk)
+        if event.status == StatusMode.DENIED:
+            message = f"The event with ID {pk}, was already in denied status."
+            return Response(status=status.HTTP_200_OK, data={"message": message})
+        
+        event.status = StatusMode.ACCEPTED
+        event.save()
+        message = f"The event with ID {pk}, is now in denied status."
+        return Response(data={"message": message}, status=status.HTTP_200_OK)
